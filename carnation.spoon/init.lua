f51ed63608e0077ac0a62b11bca48d00fe93d397
Luna <git@l4.pm>
2021-08-14 17:08:58 -0300

    add udp sending logic

--- carnation.spoon/init.lua
+++ carnation.spoon/init.lua
@@ -5,10 +5,21 @@ obj.version = "0.1"
 obj.author = "Luna <luna@l4.pm>"
 obj.license = "Unlicense"
 
+obj.config_url = nil
+
 function obj:start()
     hs.alert.show('hello')
-    self.timer = hs.timer.doEvery(0.005, function()
+
+    if not obj.config_url then
+        hs.alert.show('fucked up')
+        return
+    end
+
+    self.timer = hs.timer.doEvery(1, function()
         local pos = hs.mouse.absolutePosition()
+
+        -- find out mbpro screen dimensions
+        client = hs.socket.udp.new():send("denis", obj.config_url, 6699)
     end)
 end
 
