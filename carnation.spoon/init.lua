926de2203dca22c08e477d9d7cc0dc115ebb9d66
Luna <git@l4.pm>
2021-08-15 18:06:09 -0300

    add funny ParamAngleZ animation

--- carnation.spoon/init.lua
+++ carnation.spoon/init.lua
@@ -27,6 +27,10 @@ local function calcAngles(pos)
   return {x=resX, y=resY}
 end
 
+function obj:_send_msg(msg)
+    hs.socket.udp.new():send(msg, obj.config_url, 6699)
+end
+
 function obj:start()
     hs.alert.show('hello')
 
@@ -45,8 +49,52 @@ function obj:start()
         local pos = hs.mouse.absolutePosition()
         local angle = calcAngles(pos)
         local string = "!angles "..angle.x.." "..angle.y
-        client = hs.socket.udp.new():send(string, obj.config_url, 6699)
+        obj:_send_msg(string)
     end):start()
+
+    -- animation example, do cmd+ctrl+z to do ParamAngleZ animation
+
+    obj.__funny_state = {toggle = false, num = 0, op = 'plus'}
+    obj.__funny_timer = hs.timer.doEvery(0.006, function()
+        if not obj.__funny_state.toggle then
+            return
+        end
+
+        -- to animate angleZ, we have both the number we're currenly on
+        -- and the operation we're supossed to do on each tick
+
+        -- the animation also happens beyond the bounds of angleZ, so there's a little
+        -- bit of a holding time between each angleZ edge (-30 and 30), that makes
+        -- the animation feel more fluid and not mechanical
+
+        -- values beyond 30 here determine the holding bounds of each edge
+        if obj.__funny_state.num >= 40 and obj.__funny_state.op == 'plus' then
+            obj.__funny_state.op = 'minus'
+        elseif obj.__funny_state.num <= -40 and obj.__funny_state.op == 'minus' then
+            obj.__funny_state.op = 'plus'
+        end
+
+        -- '1' here is the amount angleZ will change per tick
+        if obj.__funny_state.op == 'plus' then
+            obj.__funny_state.num = obj.__funny_state.num + 1
+        else
+            obj.__funny_state.num = obj.__funny_state.num - 1
+        end
+
+        if obj.__funny_state.num >= -30 and obj.__funny_state.num <= 30 then
+            obj:_send_msg("set ParamAngleZ "..obj.__funny_state.num)
+        end
+    end)
+
+    hs.hotkey.bind({"cmd", "ctrl"}, "z", function()
+        obj.__funny_state.toggle = not obj.__funny_state.toggle
+        if obj.__funny_state.toggle then
+            hs.alert.show('doing funnies')
+        else
+            hs.alert.show('stopping funnies')
+            obj:_send_msg("set ParamAngleZ 0")
+        end
+    end)
 end
 
 
