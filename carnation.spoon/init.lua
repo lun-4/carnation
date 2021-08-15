7b7df217b1877096db686cfaa1bd56179fb6ac50
Luna <git@l4.pm>
2021-08-15 15:53:19 -0300

    use affine transformation for coord translation

--- carnation.spoon/init.lua
+++ carnation.spoon/init.lua
@@ -8,42 +8,23 @@ obj.license = "Unlicense"
 obj.config_url = nil
 obj.screen_dimensions = nil
 
-top = 0
--- TODO: make those values work for my mbpro
-bottom = 1079
-right = 1919
-left = 0
-
 local function calcAngles(pos)
-    local origin_x = (obj.screen_dimensions[1] / 2)
-    local x_offset = pos.x - origin_x
+  local screenWidth = obj.screen_dimensions[1]
+  local screenHeight = obj.screen_dimensions[2]
 
-    -- TODO understand the maths here and fix it
-    local left_range = origin_x - left
-    local right_range = right - origin_x
+  local inX = pos.x / screenWidth
+  local inY = pos.y / screenHeight
 
-    local angle_x = nil
+  local absX = inX
+  local absY = 1.0 - inY
 
-    if x_offset > 0 then
-        angle_x = 30 * x_offset / right_range
-    else
-        angle_x = 30 * x_offset / left_range
-    end
+  local mX = absX * 60.0
+  local mY = absY * 60.0
 
-    local origin_y = (obj.screen_dimensions[2] / 2)
-    local top_range = origin_y - top
-    local bottom_range = bottom - origin_x
-    local y_offset = pos.y - origin_y
+  local resX = mX - 30.0
+  local resY = mY - 30.0
 
-    local angle_y = nil
-
-    if y_offset > 0 then
-        angle_y = -30 * y_offset / bottom_range
-    else
-        angle_y = -30 * y_offset / top_range
-    end
-
-    return {x=angle_x, y=angle_y}
+  return {x=resX, y=resY}
 end
 
 function obj:start()
@@ -67,5 +48,6 @@ function obj:start()
         client = hs.socket.udp.new():send(string, obj.config_url, 6699)
     end):start()
 end
+
 
 return obj
